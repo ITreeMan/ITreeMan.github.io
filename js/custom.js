f1bc9e03ffdcb7f80f840d3f05832cda17ac175f
ITreeMan <p_anusorn@kkumail.com>
2019-01-02 20:42:12 +0700

    no message

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,49 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  * File Description: Place here your custom scripts
  */
+
+
+
+// Skill bar .js
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},3000);
+	});
+});
+
+
+
+
+// chang pic
+
+function changeImageOne() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("images/section-image-2.png")) {
+        image.src = "images/section-image-1.png";
+    }
+    else {
+        image.src = "images/section-image-2.png";
+    }
+}
+
+function changeImageTwo() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("images/section-image-3.png")) {
+        image.src = "images/section-image-1.png";
+    }
+    else {
+        image.src = "images/section-image-3.png";
+    }
+}
+
+function changeImageThree() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("images/section-image-2.png")) {
+        image.src = "images/section-image-1.png";
+    }
+    else {
+        image.src = "images/section-image-2.png";
+    }
+}
